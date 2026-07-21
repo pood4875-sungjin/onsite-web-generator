df7c36352a4f7ded9c96972e411fe90eb7e50a3c
김성진 <kimsungjin@gimseongjin-ui-MacBookPro.local>
2026-07-21 19:41:15 +0900

    feat(core): PageDoc shape + validatePageDoc()

---
+++ core/pagedoc.js
@@ -0,0 +1,28 @@
+/**
+ * @typedef {Object} PageDoc
+ * @property {Object} [sharedFacts]
+ * @property {SectionInstance[]} sections
+ *
+ * @typedef {Object} SectionInstance
+ * @property {string} type
+ * @property {string} [variant]
+ * @property {Object} [slotValues]
+ */
+
+/** @param {any} doc @returns {{ok: boolean, errors: string[]}} */
+export function validatePageDoc(doc) {
+  const errors = [];
+  if (!doc || typeof doc !== 'object') {
+    return { ok: false, errors: ['pageDoc must be an object'] };
+  }
+  if (!Array.isArray(doc.sections)) {
+    errors.push('pageDoc.sections must be an array');
+  } else {
+    doc.sections.forEach((s, i) => {
+      if (!s || typeof s.type !== 'string' || !s.type) {
+        errors.push(`sections[${i}].type must be a non-empty string`);
+      }
+    });
+  }
+  return { ok: errors.length === 0, errors };
+}
